9eb9a90f39f35513c51b18669401ba3ab0a52b8f
Nicholas Chao <nchao@forrester.com>
2020-01-23 23:06:00 -0500

    tidyvec obj started

---
+++ source/python/TidyVec.py
@@ -0,0 +1,36 @@
+"""
+TidyVec Object provides the source of
+word vectors and other nlp tools for R,
+through reticulate.
+"""
+
+import numpy as np
+import spacy
+
+
+class TidyVec:
+
+    '''
+    A tidy vec object represents an R tibble column.
+        text_col = text column coming from an R tibble
+        model = which (spacy) word embedding movel to use
+    '''
+
+    def __init__(self, text_col, model="en_core_web_md"):
+        self.text = text_col
+        self.model = spacy.load(model)
+        self.docs = [self.model(doc) for doc in self.text]
+
+    """
+    Vectorizes text column from R tibble..
+    """
+
+    def vectorize(self, text):
+        return [doc.vector for doc in self.docs]
+
+    """
+    Lemmatizes text column from R tibble
+    """
+
+    def lemmatize(self, text):
+        return [doc.lemma_ for doc in self.docs]
