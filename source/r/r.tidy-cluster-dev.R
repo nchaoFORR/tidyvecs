baa259c5ca073240f7ebe2ce1a8bb8f4001feea5
Nicholas Chao <nchao@forrester.com>
2020-01-23 23:07:02 -0500

    basic tidycluster with kmeans built

--- source/r/r.tidy-cluster-dev.R
+++ source/r/r.tidy-cluster-dev.R
@@ -3,12 +3,23 @@ library(tidyverse)
 data('iris')
 
 ### Create clusters of tidy data
-cluster_data <- function(df, cols = NA, algo = 'k-means', ...) {
+cluster_data <- function(df, cols = NA, algo = 'k-means', centers) {
 
   # If cols is NA, grab all numeric calls, otherwise grab
   # specified cols
   if(is.na(cols)) df <- df[, map_lgl(df, is.numeric)]
   else df <- df[, map_lgl(names(df), ~ . %in% cols)]
-  return(df)
+  
+  if(algo == 'k-means') {
+    cluster_model <- kmeans(df, centers=centers)
+    df <-
+      df %>% 
+      mutate(cluster = cluster_model$cluster)
+  }
 
+  return(df)
+  
 }
+
+
+cluster_data(iris, centers=3)
